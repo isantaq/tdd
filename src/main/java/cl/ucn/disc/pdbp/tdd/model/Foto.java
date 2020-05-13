73c06d5eb5b70764efd4218313ef957c2b7a160a
Ignacio <ignaciosantanderq12@hotmail.com>
2020-05-13 12:50:01 -0400

    feat(Foto): Foto new Class
    Agregada nueva clase Foto

---
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Foto.java
@@ -0,0 +1,16 @@
+package cl.ucn.disc.pdbp.tdd.model;
+
+/**
+ * Clase foto
+ * @author Ignacio Santander Quiñones
+ */
+public class Foto {
+
+    private Long idFoto;
+
+    private String urlNombre;
+
+    public Foto(String urlNombre){
+        this.urlNombre = urlNombre;
+    }
+}
